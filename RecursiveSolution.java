7edbbb2566bb76eb8d6d2dc9a212934d8adccc4d
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-03-26 17:48:41 -0700

    Recursive Java Solution

---
+++ RecursiveSolution.java
@@ -0,0 +1,26 @@
+public class RecursiveSolution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode a, TreeNode b) {
+        if (root == null) {
+            return null;
+        }
+
+        // If the root is one of a or b, then it is the LCA
+        if (root == a || root == b) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, a, b);
+        TreeNode right =lowestCommonAncestor(root.right, a, b);
+
+        // If both TreeNodes lie in left or right then their LCA is in left or right,
+        // Otherwise root is their LCA
+        if (left != null && right != null) {
+            return root;
+        }
+
+        return (left != null) ? left : right;
+    }
+
+
+
+}
