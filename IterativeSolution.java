4f0f65854a88455bbfd28ea4c6906a0fdfe57c90
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-03-26 18:38:49 -0700

    Iterative Java Solution

--- IterativeSolution.java
+++ IterativeSolution.java
@@ -1,7 +1,7 @@
 public class IterativeSolution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        int max = Math.max(p.val, q.val);
-        int min = Math.min(p.val, q.val);
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode a, TreeNode b) {
+        int max = Math.max(a.val, b.val);
+        int min = Math.min(a.val, b.val);
         while (max<root.val||min>root.val) {
             root=root.val<min?root.right:root.left;
         }
